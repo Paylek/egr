9cb2280a92feb0f5630ec45300546f17fe28fa28
Pavel <pggsviryd@gmail.com>
2024-12-19 18:15:41 +0300

    first commit

---
+++ src/main/java/com/sviryd/egr/EgrApplication.java
@@ -0,0 +1,13 @@
+package com.sviryd.egr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EgrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EgrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sviryd/egr/EgrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sviryd.egr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EgrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
